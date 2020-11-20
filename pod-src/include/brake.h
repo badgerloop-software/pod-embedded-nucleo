557578287a1a49854feaff02c98e6f591cdf6aa0
bennyE31 <bennye3797@gmail.com>
2020-11-20 14:46:04 -0600

    comments

--- pod-src/include/brake.h
+++ pod-src/include/brake.h
@@ -1,9 +1,33 @@
 #ifndef __BRAKE_H__
 #define __BRAKE_H__
 
+/* initBraking
+* Pings IOX 
+* Writes solenoids high for 2 seconds and then writes low
+*/
 int initBraking(void);
+
+
+
+/* writePin
+* Writes high or low to a designated pin
+* Pin numbers (0-7)=GPIOA_0-7 and numbers (8-15)=GPIOB_0-7
+* High is 1 low is 0
+*/
 int writePin(int, int);
+
+/* readPin
+*  reads the value of a designated pin and prints to console
+*/
 int readPin(int);
+
+/* testBrakWrite
+* writes all B pins low then high and returns 1 if error occurs
+*/
 uint16_t testBrakeWrite(void);
+
+/* testBrakRead
+* reads from all A pins and returns 1 if error occurs
+*/
 uint16_t testBrakeRead(void);
 #endif
--- pod-src/src/brake.cpp
+++ pod-src/src/brake.cpp
@@ -6,6 +6,11 @@ extern I2C i2c;
 
 static Iox iox(&i2c, 0x20);
 
+
+/* initBraking
+* Pings IOX 
+* Writes solenoids high for 2 seconds and then writes low
+*/
 int initBraking() {
     if(iox.ping() != 0) return 1;
     for (int i = 8; i < 16; i++){
@@ -18,14 +23,18 @@ int initBraking() {
     return 0;
 }
 
-// (0-7)=GPIOA_0-7, (8-15)=GPIOB_0-7
+/* writePin
+* Writes high or low to a designated pin
+* Pin numbers (0-7)=GPIOA_0-7 and numbers (8-15)=GPIOB_0-7
+* High is 1 low is 0
+*/
 int writePin(int pin, int val){
     I2C i2c(PB_7, PB_6);
     Iox iox(&i2c, 0x20);
 
     if (iox.setDir((IoxPin)pin, DIR_OUT)){
             printf("ERROR SETTING DIR\n\r");
-            return -1;
+            return 1;
     }
     if (iox.write((IoxPin)pin, (IoxVal)val)){ 
             printf("ERROR WRITING PIN\n\r");
@@ -35,7 +44,9 @@ int writePin(int pin, int val){
 }
 
 
-
+/* readPin
+*  reads the value of a designated pin and prints to console
+*/
 int readPin(int pin){
     I2C i2c(PB_7, PB_6);
     Iox iox(&i2c, 0x20);
@@ -48,7 +59,9 @@ int readPin(int pin){
 }
 
 
-
+/* testBrakWrite
+* writes all B pins low then high and returns 1 if error occurs
+*/
 uint16_t testBrakeWrite(){
     for (int i = 8; i < 16; i++){
         if(writePin(i, 0)){
@@ -62,7 +75,9 @@ uint16_t testBrakeWrite(){
     }
     return 0;
 }
-
+/* testBrakRead
+* reads from all A pins and returns 1 if error occurs
+*/
 uint16_t testBrakeRead(){
     for (int i = 0; i < 8; i++){
         if(readPin(i)){
